4a26709cdfc89f8025c6eae77f398cea4309c815
zhanyigang <yigangzhan@pateo.com.cn>
2022-03-25 14:37:51 +0800

    添加并完成【示例：Broadcast-普通广播】

---
+++ broadcast-normal/src/main/java/net/bi4vmr/study/broadcast_normal/MainActivity.java
@@ -0,0 +1,64 @@
+package net.bi4vmr.study.broadcast_normal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMsg;
+    private Button btSend;
+
+    private MyReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMsg = findViewById(R.id.tv_msg);
+        btSend = findViewById(R.id.bt_send);
+
+        // 创建过滤器，填写要接收的广播类型。
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("net.bi4vmr.MY_BROADCAST_MESSAGE");
+        // 创建接收器实例，并进行动态注册。
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+
+        // 发送广播按钮
+        btSend.setOnClickListener(v -> {
+            // 创建Intent实例，设置Action，并携带额外的数据。
+            Intent intent = new Intent();
+            intent.setAction("net.bi4vmr.MY_BROADCAST_MESSAGE");
+            intent.putExtra("MSG", "MyBroadcast.");
+            // 发送广播
+            sendBroadcast(intent);
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 组件被销毁时，注销广播接收器。
+        unregisterReceiver(receiver);
+    }
+
+    static class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String msg = intent.getStringExtra("MSG");
+            // 接收到广播时，需要执行的操作。
+            Log.i("myapp", "Action:" + action + "，携带数据：" + msg);
+        }
+    }
+}
