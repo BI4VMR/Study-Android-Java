e7bfc2040626b43f99c036ecb25c3251d10b93da
zhanyigang <yigangzhan@pateo.com.cn>
2022-03-28 09:40:02 +0800

    修改【示例：Broadcast-普通广播】
    1.将业务操作调整至"super.onPause();"之前，防止出现空指针异常。
    2.将消息中携带的数据更新至UI控件。

--- broadcast-normal/src/main/java/net/bi4vmr/study/broadcast_normal/MainActivity.java
+++ broadcast-normal/src/main/java/net/bi4vmr/study/broadcast_normal/MainActivity.java
@@ -2,6 +2,7 @@ package net.bi4vmr.study.broadcast_normal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 创建过滤器，填写要接收的广播类型。
         IntentFilter filter = new IntentFilter();
-        filter.addAction("net.bi4vmr.MY_BROADCAST_MESSAGE");
+        filter.addAction("net.bi4vmr.MY_BROADCAST");
         // 创建接收器实例，并进行动态注册。
         receiver = new MyReceiver();
         registerReceiver(receiver, filter);
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btSend.setOnClickListener(v -> {
             // 创建Intent实例，设置Action，并携带额外的数据。
             Intent intent = new Intent();
-            intent.setAction("net.bi4vmr.MY_BROADCAST_MESSAGE");
+            intent.setAction("net.bi4vmr.MY_BROADCAST");
             intent.putExtra("MSG", "MyBroadcast.");
             // 发送广播
             sendBroadcast(intent);
@@ -47,17 +48,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
-        super.onPause();
         // 组件被销毁时，注销广播接收器。
         unregisterReceiver(receiver);
+        super.onPause();
     }
 
-    static class MyReceiver extends BroadcastReceiver {
+    class MyReceiver extends BroadcastReceiver {
+        @SuppressLint("SetTextI18n")
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             String msg = intent.getStringExtra("MSG");
             // 接收到广播时，需要执行的操作。
+            tvMsg.setText("携带数据：" + msg);
             Log.i("myapp", "Action:" + action + "，携带数据：" + msg);
         }
     }
