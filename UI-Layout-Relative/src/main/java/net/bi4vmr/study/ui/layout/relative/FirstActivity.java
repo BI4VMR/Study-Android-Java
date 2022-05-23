dfeeb102740970fe03b88f50e38b6a77f116a4ff
BI4VMR <bi4vmr@qq.com>
2022-05-23 23:00:47 +0800

    新增【示例：UI-布局-相对布局】
    1.编写示例代码。

---
+++ UI-Layout-Relative/src/main/java/net/bi4vmr/study/ui/layout/relative/FirstActivity.java
@@ -0,0 +1,14 @@
+package net.bi4vmr.study.ui.layout.relative;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_01);
+    }
+}
