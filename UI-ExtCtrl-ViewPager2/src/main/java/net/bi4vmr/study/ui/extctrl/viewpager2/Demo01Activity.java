39c6d2e2212cbeffc05ea9fa7a1833070cecc401
BI4VMR <bi4vmr@qq.com>
2022-06-14 00:20:47 +0800

    c5cb6132-3571-4fa6-b4d1-b54472b92e18

--- UI-ExtCtrl-ViewPager2/src/main/java/net/bi4vmr/study/ui/extctrl/viewpager2/Demo01Activity.java
+++ UI-ExtCtrl-ViewPager2/src/main/java/net/bi4vmr/study/ui/extctrl/viewpager2/Demo01Activity.java
@@ -1,9 +1,12 @@
 package net.bi4vmr.study.ui.extctrl.viewpager2;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import net.bi4vmr.study.ui.extctrl.viewpager2.adapter.Adapter01;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +18,6 @@ public class Demo01Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_01);
 
-        ViewPager2 viewPager2 = findViewById(R.id.vp2Content);
 
         // 创建测试页面
         List<TestFragment> pages = new ArrayList<>();
@@ -24,8 +26,32 @@ public class Demo01Activity extends AppCompatActivity {
         }
 
         // 为ViewPager设置适配器
+        ViewPager2 viewPager2 = findViewById(R.id.vp2Content);
         Adapter01 adapter = new Adapter01(this, pages);
         viewPager2.setAdapter(adapter);
+
+        // 设置页面改变监听器
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d("myapp", "onPageScrolled:" + position);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d("myapp", "onPageSelected:" + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.d("myapp", "onPageScrollStateChanged:" + state);
+            }
+        });
+
+        /*
+         * 设置滑动方向为垂直
+         */
+        // viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
 
         /*
          * 设置预加载页面的数量
@@ -33,11 +59,6 @@ public class Demo01Activity extends AppCompatActivity {
          * 默认值为"-1"，即不进行预加载；设置为N个页面时，系统除了加载当前页面，还会额外分别加载该页面两侧的N个页面。
          */
         // viewPager2.setOffscreenPageLimit(1);
-
-        /*
-         * 设置滚动方向
-         */
-        // viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
 
         /*
          * 是否允许用户触摸翻页
--- UI-ExtCtrl-ViewPager2/src/main/java/net/bi4vmr/study/ui/extctrl/viewpager2/adapter/Adapter01.java
+++ UI-ExtCtrl-ViewPager2/src/main/java/net/bi4vmr/study/ui/extctrl/viewpager2/adapter/Adapter01.java
@@ -1,27 +1,33 @@
-package net.bi4vmr.study.ui.extctrl.viewpager2;
+package net.bi4vmr.study.ui.extctrl.viewpager2.adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import net.bi4vmr.study.ui.extctrl.viewpager2.TestFragment;
+
 import java.util.List;
 
 public class Adapter01 extends FragmentStateAdapter {
 
+    // 数据源List
     private final List<TestFragment> pages;
 
+    // 构造方法
     public Adapter01(FragmentActivity activity, List<TestFragment> pages) {
         super(activity);
         this.pages = pages;
     }
 
+    /* 创建Fragment实例 */
     @NonNull
     @Override
     public Fragment createFragment(int position) {
         return pages.get(position);
     }
 
+    /* 获取页面数量 */
     @Override
     public int getItemCount() {
         return pages.size();
